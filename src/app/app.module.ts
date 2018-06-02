09a577906d5af3bc68cd866d297da302991910f1
jobizzness <jobizzness@gmail.com>
2018-06-03 05:27:36 +0800

    angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule, MatSidenavModule, MatIconModule, MatCheckboxModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatCheckboxModule,
+
+    FormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
