540d7b507fcfa38f6a84d0773a819b4861b1526e
jobizzness <jobizzness@gmail.com>
2018-06-03 08:57:30 +0800

    building dashboard components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule, MatSidenavModule, MatIconModule, MatCheckboxModule } from '@angular/material';
-
+import { RouterModule } from "@angular/router";
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SummaryItemComponent } from './summary-item/summary-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    SummaryItemComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-
+    RouterModule.forRoot([
+      {path: '', component: DashboardComponent},
+      { path: '**', component: DashboardComponent }
+    ]),
     MatToolbarModule,
     MatSidenavModule,
     MatIconModule,
---
+++ src/app/summary-item/summary-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'summary-item',
+  templateUrl: './summary-item.component.html',
+  styleUrls: ['./summary-item.component.scss']
+})
+export class SummaryItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
